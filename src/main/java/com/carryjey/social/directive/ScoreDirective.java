4b67b7ccd14a50779be07cabd298b265e404e9a7
CarryJey <857516673@qq.com>
2019-05-14 23:07:39 +0800

    添加TemplateDirectiveModel注释说明-排行榜优化

--- src/main/java/com/carryjey/social/directive/ScoreDirective.java
+++ src/main/java/com/carryjey/social/directive/ScoreDirective.java
@@ -26,6 +26,14 @@ public class ScoreDirective implements TemplateDirectiveModel {
     @Autowired
     private UserService userService;
 
+    /**
+     * 通过实现FreeMarker的TemplateDirectiveModel就在后端实现了一个自定义的宏，
+     * 这个宏的功能很简单，只是根据给定的参数将排行数据“users”放到model中去，
+     * 然后模板页中就可以使用这个变量了。
+     *
+     * 无需在各个controller的各个接口中去重复的向model中添加所需的排行数据，
+     * 而是当FreeMarker渲染模板页时遇到相应的宏它可以回到后端去调用相应的方法取到所需的数据
+     */
     @Override
     public void execute(
         Environment environment, Map map, TemplateModel[] templateModels, TemplateDirectiveBody templateDirectiveBody)
